244015b99f482cae6eb4bb27a451b165935ae8a8
s.knoll@bkr.de <s.knoll@bkr.de>
2026-01-04 12:28:02 +0100

    add plantdbcontext

---
+++ Context/PlantDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+using PlantApp.Api.Models;
+
+namespace PlantApp.Api.Context
+{
+    public class PlantDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public PlantDbContext(DbContextOptions<PlantDbContext> options) 
+            : base(options) { }
+
+        public DbSet<UserPlant> UserPlants => Set<UserPlant>();
+        public DbSet<Models.Task> Tasks => base.Set<Models.Task>();
+    }
+}
