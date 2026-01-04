b34c7d358dce9c2d20aeda522836076b81fda207
s.knoll@bkr.de <s.knoll@bkr.de>
2026-01-04 12:24:21 +0100

    add user

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace PlantApp.Api.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public int Id { get; set; }
+        public string Email { get; set; } = null!;
+
+        public ICollection<UserPlant> Plants { get; set; } = new List<UserPlant>();
+    }
+}
