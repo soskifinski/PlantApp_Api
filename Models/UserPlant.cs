1bc35faebe29800cc7e2ee45052fa7fe41e47cc1
s.knoll@bkr.de <s.knoll@bkr.de>
2026-01-04 12:24:34 +0100

    add userplant

---
+++ Models/UserPlant.cs
@@ -0,0 +1,14 @@
+﻿namespace PlantApp.Api.Models
+{
+    public class UserPlant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public bool Edible { get; set; }
+
+        public int UserId { get; set; }
+        public ApplicationUser User { get; set; } = null!;
+
+        public ICollection<TaskItem> Tasks { get; set; } = new List<TaskItem>();
+    }
+}
