e158f9434163346acb2f1ad29132857792bbc68c
s.knoll@bkr.de <s.knoll@bkr.de>
2026-01-04 12:29:43 +0100

    add plantscontroller

---
+++ Controller/PlantsController.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Claims;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using PlantApp.Api.Context;
+using PlantApp.Api.Models;
+
+namespace PlantApp.Api.Controller
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/plants")]
+    public class PlantsController : ControllerBase
+    {
+        private readonly PlantDbContext _db;
+
+        public PlantsController(PlantDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<UserPlant>> Get()
+        {
+            var userId = int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+            return await _db.UserPlants.Where(p => p.UserId == userId).ToListAsync();
+        }
+    }
+
+}
